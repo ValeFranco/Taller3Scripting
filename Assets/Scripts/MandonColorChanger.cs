90e2fd88c772675f53d6f475eaadd9918c110a11
LunarBullet <lunarbulletstudios@gmail.com>
2022-05-05 19:31:32 -0500

    Punto 2 elegante y funcional

---
+++ Assets/Scripts/MandonColorChanger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    Renderer renderer;
+    [SerializeField] Material[] differentColorMaterials = new Material[4];
+    private void Awake() => renderer = GetComponent<Renderer>();
+    private void Start() => MandonEventManager.OnButtonClicked += ChangeColor;
+    void ChangeColor() => renderer.material = differentColorMaterials[MandonEventManager.cyclicCounter-1];
+
+}
---
+++ Assets/Scripts/MandonEventManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MandonEventManager : MonoBehaviour
+{
+    public delegate void MyClickAction();
+    public static event MyClickAction OnButtonClicked;
+    public static int cyclicCounter { get; private set; } = 0;
+
+    void Start() => OnButtonClicked += CyclicNumberCounter;
+
+    void Update() => MouseInput();
+    void MouseInput() { if (Input.GetMouseButtonDown(0)) OnButtonClicked?.Invoke(); }
+
+    void CyclicNumberCounter()
+    {
+        cyclicCounter = cyclicCounter == 4 ? cyclicCounter = 1 : cyclicCounter;
+        cyclicCounter++;
+    }
+
+}
